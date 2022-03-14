d9da1dfe3ddbcf1d9d9c7eb9cdfc08eeeec39cf1
roka0105 <cocapepsi123@daum.net>
2022-03-14 22:23:42 +0900

    1.유니티 자습 2.서버 수업 관련 업데이트

---
+++ school study/3_1/GameTool/class_data/EX_FSMUGUI/Assets/Scenes/02_FSMCodeStack/Script/Code_StateStackMachine.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Du3Project
+{
+	public class Code_StateStackMachine
+	{
+        Dictionary<string, IState> mStates = new Dictionary<string, IState>();
+        Stack<IState> m_Stack = new Stack<IState>();
+
+        public void Update(float elapsedTime)
+        {
+            IState top = m_Stack.Peek();
+            top.UpdateState(elapsedTime);
+        }
+
+        public void Render()
+        {
+            IState top = m_Stack.Peek();
+            top.Render();
+        }
+
+        public IState Push(string name)
+        {
+            if(m_Stack.Count > 0)
+            {
+                IState top = m_Stack.Peek();
+                top.OnExit();
+            }
+            
+
+            IState state = mStates[name];
+            m_Stack.Push(state);
+            state.OnEnter(null);
+
+            return state;
+        }
+
+        public IState Pop()
+        {
+            if (m_Stack.Count <= 1)
+            {
+                return null;
+            }
+
+            IState pop = m_Stack.Pop();
+            pop.OnExit();
+
+            IState top = m_Stack.Peek();
+            top.OnEnter(null);
+
+            return pop;
+        }
+
+        public void Add(string p_name, IState p_state)
+        {
+            if( mStates.ContainsKey(p_name) )
+            {
+                mStates[p_name] = p_state;
+            }
+            else
+            {
+                mStates.Add(p_name, p_state);
+            }
+            
+        }
+
+	}
+}
---
+++ school study/3_1/GameTool/class_data/EX_FSMUGUI/Assets/Scenes/02_FSMCodeStack/Script/UIStatckManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Du3Project
+{
+    // 기본적으로 완성된 소스가 아님 pop으로 해서 기존에 있던것들을 전부 사라지도록 해야지 되는데 현재 것은 그런 방식이 아님
+	public class UIStatckManager : MonoBehaviour
+	{
+        [Header("[스테이트용]")]
+        public UI_FieldMap FieldMapCom = null;
+        public UI_WorldMap WorldCom = null;
+        public UI_Option OptionCom = null;
+        public UI_Battle BattleCom = null;
+
+
+
+        public E_UIState CurrentUIState = E_UIState.worldmap;
+        protected Code_StateStackMachine m_CodeStatckMachine = new Code_StateStackMachine();
+
+
+        public void ChangeState(E_UIState p_change)
+        {
+            IState pushstate = m_CodeStatckMachine.Push(p_change.ToString());
+            //(pushstate as MonoBehaviour).gameObject.SetActive(true);
+
+
+        }
+
+        void InitStateDatas()
+        {
+            m_CodeStatckMachine.Add(E_UIState.battle.ToString(), BattleCom);
+            m_CodeStatckMachine.Add(E_UIState.worldmap.ToString(), WorldCom);
+            m_CodeStatckMachine.Add(E_UIState.option.ToString(), OptionCom);
+            m_CodeStatckMachine.Add(E_UIState.fieldmap.ToString(), FieldMapCom);
+
+            BattleCom.gameObject.SetActive(false);
+            WorldCom.gameObject.SetActive(false);
+            OptionCom.gameObject.SetActive(false);
+            FieldMapCom.gameObject.SetActive(false);
+        }
+
+        void Awake()
+        {
+            InitStateDatas();
+            ChangeState(CurrentUIState);
+
+        }
+
+        void Start()
+		{
+			
+		}
+
+        void Update()
+        {
+            m_CodeStatckMachine.Update(Time.time);
+            m_CodeStatckMachine.Render();
+        }
+
+
+
+        [ContextMenu("[UI추가]")]
+        void Editor_AddChangeUI()
+        {
+            ChangeState(CurrentUIState);
+
+        }
+
+
+        [ContextMenu("[기존UI취소]")]
+        void Editor_PopChangeUI()
+        {
+            IState popstate = m_CodeStatckMachine.Pop();
+            (popstate as MonoBehaviour).gameObject.SetActive(false);
+
+        }
+
+    }
+}
