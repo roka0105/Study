10dc1ecbd4207f98e281ec2501775149c13456d7
roka0105 <cocapepsi123@daum.net>
2022-04-12 09:08:51 +0900

    no message

--- school study/3_1/server/자습 및 과제/TCP,UDP 함께 사용한 채팅서버/client/client/client.cpp
+++ school study/3_1/server/자습 및 과제/TCP,UDP 함께 사용한 채팅서버/client/client/client.cpp
@@ -1,7 +1,8 @@
 #pragma comment (lib,"ws2_32")
 #include <winsock2.h>
 #include <stdio.h>
-
+#include "resource.h"
+#include <ws2tcpip.h>
 #define BUFSIZE 512
 #define SERVERIP "127.1.1.1"
 #define SERVERPORT 9000
@@ -15,6 +16,7 @@ enum class PROTOCOL
 	MENU,
 	ADDR,
 	CHATTING,
+	END,
 	MAX
 };
 enum class STATE
@@ -31,6 +33,12 @@ typedef struct NetworkPacket
 	char Data[BUFSIZE];
 	PROTOCOL protocol;
 };
+typedef struct UdpPacket
+{
+	PROTOCOL protocol;
+	char name[BUFSIZE];
+	char chat[BUFSIZE];
+};
 typedef struct UserInfo
 {
 	char nickname[BUFSIZE];
@@ -38,6 +46,11 @@ typedef struct UserInfo
 	SOCKADDR_IN addr;
 };
 
+HWND _hDlg, NextroomBtn, OutRoomBtn, SendBtn, Edit, ShowEdit;
+HANDLE hStartEvent, hSendEvent;
+UserInfo user;
+bool flag = false;
+PROTOCOL protocol;
 void err_quit(const char* msg)
 {
 	LPVOID lpMsgBuf;
@@ -89,7 +102,7 @@ bool RecvPacket(SOCKET s, char* recvbuf)
 
 	return true;
 }
-void GetProtocol(const char* buf,PROTOCOL* protocol)
+void GetProtocol(const char* buf, PROTOCOL* protocol)
 {
 	const char* ptr = buf;
 	memcpy(protocol, buf, sizeof(PROTOCOL));
@@ -97,52 +110,119 @@ void GetProtocol(const char* buf,PROTOCOL* protocol)
 int PackPacking(char* buf, PROTOCOL protocol, const char* msg)
 {
 	char* ptr = buf + sizeof(int);
-	int strsize = strlen(msg);
+	int strsize = 0; 
 	int size = 0;
 	memcpy(ptr, &protocol, sizeof(PROTOCOL));
 	size += sizeof(PROTOCOL);
 	ptr += sizeof(PROTOCOL);
 	if (msg != NULL)
 	{
+		strsize= strlen(msg);
 		memcpy(ptr, &strsize, sizeof(int));
 		ptr += sizeof(int);
 		size += sizeof(int);
 
-		memcpy(ptr, msg, strsize);
-		ptr = buf;
+		memcpy(ptr, msg, strsize);	
 		size += strsize;
 	}
-	
+    ptr = buf;
 	memcpy(ptr, &size, sizeof(int));
 	size += sizeof(int);
 	return size;
 }
-void UnPackPacket(const char* buf, char* nickname)
+int PackPacking(char* buf, PROTOCOL protocol, char* msg1, char* msg2)
 {
-	const char* ptr = buf+sizeof(PROTOCOL);
+	char* ptr = buf + sizeof(int);
+	int strsize = strlen(msg1);
+	int size = 0;
+	memcpy(ptr, &protocol, sizeof(PROTOCOL));
+	size += sizeof(PROTOCOL);
+	ptr += sizeof(PROTOCOL);
+	if (msg1 != NULL && msg2 != NULL)
+	{
+		memcpy(ptr, &strsize, sizeof(int));
+		ptr += sizeof(int);
+		size += sizeof(int);
+
+		memcpy(ptr, msg1, strsize);
+		ptr += strsize;
+		size += strsize;
+
+		strsize = strlen(msg2);
+		memcpy(ptr, &strsize, sizeof(int));
+		ptr += sizeof(int);
+		size += sizeof(int);
+		memcpy(ptr, msg2, strsize);
+		size += strsize;
+		ptr = buf;
+	}
+
+	memcpy(ptr, &size, sizeof(int));
+	size += sizeof(int);
+	return size;
+}
+void UnPackPacket(const char* buf, char* data)
+{
+	const char* ptr = buf + sizeof(PROTOCOL);
 	int strsize = 0;
 	memcpy(&strsize, ptr, sizeof(int));
 	ptr += sizeof(int);
-	memcpy(nickname, ptr, strsize);
+	memcpy(data, ptr, strsize);
 }
+void UnPackPacket(const char* buf, char* data1, char* data2)
+{
+	const char* ptr = buf + sizeof(PROTOCOL);
+	int strsize = 0;
+	memcpy(&strsize, ptr, sizeof(int));
+	ptr += sizeof(int);
+	memcpy(data1, ptr, strsize);
+	ptr += strsize;
+	memcpy(&strsize, ptr, sizeof(int));
+	ptr += sizeof(int);
+	memcpy(data2, ptr, strsize);
+}
+void UnPackPacket(const char* buf, int* data2, char* data1)
+{
+	const char* ptr = buf + sizeof(PROTOCOL);
+	int strsize = 0;
+	memcpy(data2, ptr, sizeof(int));
+	ptr += sizeof(int);
+	memcpy(&strsize, ptr, sizeof(int));
+	ptr += sizeof(int);
+	memcpy(data1, ptr, strsize);
+}
+DWORD WINAPI ClientThread(LPVOID);
+DWORD WINAPI RecvThread(LPVOID);
 
+INT_PTR CALLBACK DlgProc1(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine, int nCmdShow)
+{
+	HANDLE hThread = CreateThread(0, NULL, ClientThread, NULL, 0, NULL);
+	hStartEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+	hSendEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+	DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc1);
+
+
+	WSACleanup();
+	return 0;
+}
+DWORD WINAPI ClientThread(LPVOID arg)
 {
 	int retval = 0;
 	WSADATA ws;
 	if (WSAStartup(MAKEWORD(2, 2), &ws) != 0)
 		return -1;
-	SOCKET sock=socket(AF_INET,SOCK_STREAM,0);
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
 	SOCKADDR_IN addr;
 	ZeroMemory(&addr, sizeof(addr));
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = inet_addr(SERVERIP);
 	addr.sin_port = htons(SERVERPORT);
 
-	int addrlen=0;
+	int addrlen = 0;
 	NetworkPacket packet;
-	UserInfo user;
+
 	ZeroMemory(&user, sizeof(user));
 	ZeroMemory(&packet, sizeof(NetworkPacket));
 	//connect
@@ -152,49 +232,310 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 	char buf[BUFSIZE];
 	int size = 0;
-	char number = 0;
+	int port = 0;
+	int strsize = 0;
+
+	SOCKET udpSock;
+	SOCKADDR_IN udpaddr;
+
+	BOOL bEnable = TRUE;
+	ip_mreq mreq;
+	SOCKADDR_IN remoteaddr;
+
+	HANDLE hRecvThread;
+	UdpPacket udppacket;
+	ZeroMemory(&udppacket, sizeof(UdpPacket));
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = inet_addr(SERVERIP);
+	serveraddr.sin_port = htons(9005);
+
+	bool end = false;
 	while (1)
 	{
+		ZeroMemory(packet.Data, BUFSIZE);
 		if (!RecvPacket(sock, packet.Data))
 		{
-			MessageBox(NULL, (LPCTSTR)"데이터받기 실패\n", (LPCTSTR)"수신실패", MB_OK);
+			MessageBox(NULL, "데이터받기 실패\n", "수신실패", MB_OK);
+			break;
 		}
 		GetProtocol(packet.Data, &packet.protocol);
 		switch (packet.protocol)
 		{
 		case PROTOCOL::INPUTNICKNAME:
+			WaitForSingleObject(hStartEvent, INFINITE);
 			//입력하시오 출력
 			ZeroMemory(buf, BUFSIZE);
 			UnPackPacket(packet.Data, buf);
-			printf("%s", buf);
 
+			SetWindowText(ShowEdit, buf);
 			//입력값과 메뉴요청 전송
 			ZeroMemory(user.nickname, BUFSIZE);
-			scanf("%s", user.nickname);
-
+			WaitForSingleObject(hSendEvent, INFINITE);
+			if (protocol == PROTOCOL::END)
+			{
+				end = true;
+				PackPacking(packet.Data, PROTOCOL::END, NULL);
+				size = send(sock, packet.Data, size, 0);
+				if (retval == SOCKET_ERROR)
+					err_display("send()");
+				break;
+			}
+			GetDlgItemText(_hDlg, IDC_EDIT1, user.nickname, BUFSIZE);
+			SendMessage(Edit, EM_SETSEL, 0, BUFSIZE);
+			SendMessage(Edit, EM_REPLACESEL, (WPARAM)TRUE, (LPARAM)"");
+			strsize = strlen(user.nickname);
 			size = PackPacking(packet.Data, PROTOCOL::MENU, user.nickname);
 			retval = send(sock, packet.Data, size, 0);
 			if (retval == SOCKET_ERROR)
 				err_display("send()");
-				break;
+			break;
 		case PROTOCOL::MENU:
 			//받아온 메뉴 출력
 			ZeroMemory(buf, BUFSIZE);
 			UnPackPacket(packet.Data, buf);
-			printf("%s", buf);
-			
-			//방 선택 및 방 정보 전송
-			
+			SetWindowText(ShowEdit, buf);
+			WaitForSingleObject(hSendEvent, INFINITE);
+			if (protocol == PROTOCOL::END)
+			{
+				end = true;
+				PackPacking(packet.Data, PROTOCOL::END,NULL);
+				size = send(sock, packet.Data, size, 0);
+				if (retval == SOCKET_ERROR)
+					err_display("send()");
+				break;
+			}
+			ZeroMemory(buf, BUFSIZE);
 			ZeroMemory(packet.Data, BUFSIZE);
-			scanf("%c", &number);
-			PackPacking(packet.Data, PROTOCOL::ADDR, &number);
+			//에딧컨트롤에서 입력값을 빼오고 edit창 비우기.
+			GetDlgItemText(_hDlg, IDC_EDIT1, buf, BUFSIZE);
+			SendMessage(Edit, EM_SETSEL, 0, BUFSIZE);
+			SendMessage(Edit, EM_REPLACESEL, (WPARAM)TRUE, (LPARAM)"");
+			//방 선택 및 방 정보 전송
+			PackPacking(packet.Data, PROTOCOL::ADDR, buf);
+			size = send(sock, packet.Data, size, 0);
+			if (retval == SOCKET_ERROR)
+				err_display("send()");
 			break;
 		case PROTOCOL::ADDR:
+			EnableWindow(OutRoomBtn, TRUE);
+			//주소받아옴
+			ZeroMemory(buf, BUFSIZE);
+			UnPackPacket(packet.Data, &port, buf);
+			strsize = GetWindowTextLength(ShowEdit);
+			//메뉴 출력했던것 문자열 지우기
+			SendMessage(ShowEdit, EM_SETSEL, 0, strsize);
+			SendMessage(ShowEdit, EM_REPLACESEL, (WPARAM)TRUE, (LPARAM)"");
+			//tcp 소켓은 그대로 두고 udp 소켓을 생성해야할거같은데
+			udpSock = socket(AF_INET, SOCK_DGRAM, 0);
+			if (udpSock == INVALID_SOCKET)
+				err_quit("socket()");
+
+			// 소켓 생성시 같은 곳에서 접근할때 커널에서 오류처리.접근을 허용
+			retval = setsockopt(udpSock, SOL_SOCKET, SO_REUSEADDR, (char*)&bEnable, sizeof(bEnable));
+			if (retval == SOCKET_ERROR)
+				err_quit("setsockopt()");
+
+			//멀티캐스트 멤버 등록
+			mreq.imr_multiaddr.s_addr = inet_addr(buf);//멀티캐스트 주소
+			mreq.imr_interface.s_addr = htonl(INADDR_ANY);//내 주소
+			retval = setsockopt(udpSock, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char*)&mreq, sizeof(mreq));
+			if (retval == SOCKET_ERROR)
+				err_quit("setsockopt()");
+
+			//recv받기 위한 바인딩.
+			ZeroMemory(&udpaddr, sizeof(udpaddr));
+			udpaddr.sin_family = AF_INET;
+			udpaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+			udpaddr.sin_port = htons(port);
+			retval = bind(udpSock, (SOCKADDR*)&udpaddr, sizeof(udpaddr));
+			if (retval == SOCKET_ERROR)
+				err_quit("bind()");
+
+			//send 주소구조체
+			ZeroMemory(&remoteaddr, sizeof(remoteaddr));
+			remoteaddr.sin_family = AF_INET;
+			remoteaddr.sin_addr.s_addr = inet_addr(buf);
+			remoteaddr.sin_port = htons(port);
+
+			//받기전용 스레드
+			hRecvThread = CreateThread(0, NULL, RecvThread, (void*)udpSock, 0, NULL);
+
+			//채팅
+			while (1)
+			{
+				//다이어로그에서 상호작용키를 누르면 활성화 (보내기,방나가기)
+				WaitForSingleObject(hSendEvent, INFINITE);
+				//방나가기 이면 while문 탈출
+				if (flag)
+					break;
+				ZeroMemory(buf, BUFSIZE);
+				ZeroMemory(packet.Data, BUFSIZE);
+				//에딧에서 글자 빼옴
+				GetDlgItemText(_hDlg, IDC_EDIT1, buf, BUFSIZE);
+				//글자수 카운트
+				strsize = GetWindowTextLength(Edit);
+				//글자수 만큼 지우기
+				SendMessage(Edit, EM_SETSEL, 0, strsize);
+				SendMessage(Edit, EM_REPLACESEL, (WPARAM)TRUE, (LPARAM)"");
+				//닉네임과 대화내용 전송
+				strcpy(udppacket.name, user.nickname);
+				strcpy(udppacket.chat, buf);
+				//remoteaddr은 멀티캐스트 주소.
+				retval = sendto(udpSock, (char*)&udppacket, sizeof(udppacket), 0, (SOCKADDR*)&remoteaddr, sizeof(remoteaddr));
+				if (retval == SOCKET_ERROR)
+					err_display("sendto()");
+			}
+			//방나가기 후 방 선택하기 또는 끝내기를 누르면 활성화.
+			WaitForSingleObject(hStartEvent, INFINITE);
+
+			//패킷에 protocol을 보내서 서버 udp에 전송
+			if (protocol == PROTOCOL::MENU)
+			{
+				udppacket.protocol = PROTOCOL::MENU;
+			}
+			else if (protocol == PROTOCOL::END)
+			{
+				udppacket.protocol = PROTOCOL::END;
+			}
+			retval = sendto(udpSock, (char*)&udppacket, sizeof(udppacket), 0, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+			if (retval == SOCKET_ERROR)
+				err_display("send()");
+
+			//서버에 전송한 protocol이 다시 recvthread로 가서 thread를 종료시켜야 wait이 풀림.
+			WaitForSingleObject(hRecvThread, INFINITE);
+	
+			//멤버탈퇴 및 소켓해제
+			retval = setsockopt(udpSock, IPPROTO_IP, IP_DROP_MEMBERSHIP, (char*)&mreq, sizeof(mreq));
+			if (retval == SOCKET_ERROR)
+				err_display("setsockopt()");
+
+	
+			closesocket(udpSock);
+			break;
+		case PROTOCOL::END:
+			end = true;
 			break;
 		}
+		if (end)
+			break;
 	}
-	
+	MessageBox(NULL, "메인스레드 종료", "종료확인", MB_OK);
+
 	closesocket(sock);
-	WSACleanup();
+	return 0;
+}
+INT_PTR CALLBACK DlgProc1(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	int strsize = 0;
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		_hDlg = hDlg;
+		SendBtn = GetDlgItem(hDlg, IDOK);
+		NextroomBtn = GetDlgItem(hDlg, IDNEXTROOM);
+		OutRoomBtn = GetDlgItem(hDlg, IDOUTROOM);
+		Edit = GetDlgItem(hDlg, IDC_EDIT1);
+		ShowEdit = GetDlgItem(hDlg, IDC_EDIT2);
+		EnableWindow(NextroomBtn, FALSE);
+		EnableWindow(SendBtn, TRUE);
+		EnableWindow(OutRoomBtn, FALSE);
+		SetEvent(hStartEvent);
+		return TRUE;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+			SetEvent(hSendEvent);
+			return TRUE;
+		case IDOUTROOM:
+			EnableWindow(NextroomBtn, TRUE);
+			EnableWindow(SendBtn, FALSE);
+			flag = true;
+			SetEvent(hSendEvent);
+			return TRUE;
+		case IDNEXTROOM:
+			protocol = PROTOCOL::MENU;
+			EnableWindow(NextroomBtn, FALSE);
+			EnableWindow(SendBtn, TRUE);
+			EnableWindow(OutRoomBtn, FALSE);
+			SetEvent(hStartEvent);
+			return TRUE;
+		case IDEND:
+			flag = true;
+			protocol = PROTOCOL::END;
+			SetEvent(hSendEvent);
+			SetEvent(hStartEvent);
+			EndDialog(hDlg, IDEND);
+			return TRUE;
+		}
+		return FALSE;
+	case WM_CLOSE:
+		flag = true;
+		protocol = PROTOCOL::END;
+		SetEvent(hSendEvent);
+		SetEvent(hStartEvent);
+		EndDialog(hDlg, WM_CLOSE);
+		return TRUE;
+	}
+	return FALSE;
+}
+DWORD WINAPI RecvThread(LPVOID arg)
+{
+	SOCKET sock = (SOCKET)arg;
+	SOCKADDR_IN multipeeraddr;
+	int peeraddrlen = sizeof(multipeeraddr);
+	UdpPacket udppacket;
+
+	int retval = 0;
+	ZeroMemory(&multipeeraddr, sizeof(multipeeraddr));
+	int txtlen = 0;
+	char buf[BUFSIZE];
+	char temp[BUFSIZE];
+	char name[BUFSIZE];
+	//timeval timeo = { 3,500000 };
+
+	//setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeo, sizeof(timeo));
+	while (1)
+	{
+		ZeroMemory(name, BUFSIZE);
+		ZeroMemory(temp, BUFSIZE);
+		ZeroMemory(buf, BUFSIZE);
+		ZeroMemory(&udppacket, sizeof(udppacket));
+		ZeroMemory(udppacket.name, BUFSIZE);
+		ZeroMemory(udppacket.chat, BUFSIZE);
+		
+		//서버로 전송한 protocol 패킷이 여기로 오면 종료되고 아니면 멀티캐스트에서 뿌리는 데이터를 받음.
+		retval = recvfrom(sock, (char*)&udppacket, sizeof(udppacket), 0, (SOCKADDR*)&multipeeraddr, &peeraddrlen);
+		if (retval == SOCKET_ERROR)
+		{
+			err_display("recvfrom()");
+			MessageBox(NULL, "스레드종료", "확인", MB_OK);
+		}
+		else
+		{
+			//서버에서 온 종료조건이 충족될 경우
+			//menu는 방나가기 후 방선택하기를 선택한 경우 반복문 탈출 및 스레드 종료
+			if (udppacket.protocol == PROTOCOL::MENU)
+			{
+				SendMessage(ShowEdit, EM_SETSEL, txtlen, txtlen);
+				SendMessage(ShowEdit, EM_REPLACESEL, (WPARAM)FALSE, (LPARAM)"스레드 종료");
+				break;
+			}//끝내기 반복문 탈출 및 스레드 종료
+			else if (udppacket.protocol == PROTOCOL::END)
+			{
+				MessageBox(NULL, "스레드종료", "확인", MB_OK);
+				return 0;
+			}
+			//위에서 안 걸릴경우 그냥 일반 채팅인 경우이므로 닉네임과 대화 출력
+			sprintf(buf, "[%s] %s \r\n", udppacket.name, udppacket.chat);
+			txtlen = GetWindowTextLength(ShowEdit);
+
+			SendMessage(ShowEdit, EM_SETSEL, txtlen, txtlen);
+			SendMessage(ShowEdit, EM_REPLACESEL, (WPARAM)FALSE, (LPARAM)buf);
+		}
+	}
+
 	return 0;
 }
--- school study/3_1/server/자습 및 과제/TCP,UDP 함께 사용한 채팅서버/server/server/server.cpp
+++ school study/3_1/server/자습 및 과제/TCP,UDP 함께 사용한 채팅서버/server/server/server.cpp
@@ -5,7 +5,7 @@
 #define BUFSIZE 512
 #define SERVERIP "127.1.1.1"
 #define SERVERPORT 9000
-#define MENUSTRING "메뉴를 선택하세요\n1.박용택방\n2.오지환방\n3.김현수방"
+#define MENUSTRING "메뉴를 선택하세요\r\n1.박용택방\r\n2.오지환방\r\n3.김현수방"
 #define NICKNAMEMSG "사용하실 닉네임 입력해주세요\n"
 
 DWORD WINAPI ClientThread(LPVOID arg);
@@ -17,6 +17,7 @@ enum class PROTOCOL
 	MENU,
 	ADDR,
 	CHATTING,
+	END,
 	MAX
 };
 enum class STATE
@@ -26,6 +27,7 @@ enum class STATE
 	MENU,
 	ADDR,
 	CHATTING,
+	END,
 	MAX
 };
 typedef struct NetworkPacket
@@ -33,8 +35,19 @@ typedef struct NetworkPacket
 	char Data[BUFSIZE];
 	PROTOCOL protocol;
 };
+typedef struct UdpPacket
+{
+	PROTOCOL protocol;
+	char name[BUFSIZE];
+	char chat[BUFSIZE];
+};
 typedef struct UserInfo
 {
+	UserInfo()
+	{
+		ZeroMemory(nickname, BUFSIZE);
+		ZeroMemory(&addr, sizeof(addr));
+	}
 	char nickname[BUFSIZE];
 	SOCKET sock;
 	SOCKADDR_IN addr;
@@ -94,13 +107,14 @@ bool RecvPacket(SOCKET s, char* recvbuf)
 int PackPacking(char* buf, PROTOCOL protocol, const char* msg)
 {
 	char* ptr = buf + sizeof(int);
-	int strsize = strlen(msg);
+	int strsize = 0; 
 	int size = 0;
 	memcpy(ptr, &protocol, sizeof(PROTOCOL));
 	size += sizeof(PROTOCOL);
 	ptr += sizeof(PROTOCOL);
 	if (msg != NULL)
 	{
+		strsize= strlen(msg);
 		memcpy(ptr, &strsize, sizeof(int));
 		ptr += sizeof(int);
 		size += sizeof(int);
@@ -113,13 +127,44 @@ int PackPacking(char* buf, PROTOCOL protocol, const char* msg)
 	size += sizeof(int);
 	return size;
 }
-void UnPackPacket(const char* buf, char* nickname)
+int PackPacking(char* buf, PROTOCOL protocol, const char* msg, int* num)
 {
-	const char* ptr = buf+sizeof(PROTOCOL);
+	char* ptr = buf + sizeof(int);
+	int strsize = strlen(msg);
+	int size = 0;
+	memcpy(ptr, &protocol, sizeof(PROTOCOL));
+	size += sizeof(PROTOCOL);
+	ptr += sizeof(PROTOCOL);
+	memcpy(ptr, num, sizeof(int));
+	size += sizeof(int);
+	ptr += sizeof(int);
+	if (msg != NULL)
+	{
+		memcpy(ptr, &strsize, sizeof(int));
+		ptr += sizeof(int);
+		size += sizeof(int);
+
+		memcpy(ptr, msg, strsize);
+		size += strsize;
+	}
+	ptr = buf;
+	memcpy(ptr, &size, sizeof(int));
+	size += sizeof(int);
+
+	return size;
+}
+void UnPackPacket(const char* buf, char* msg)
+{
+	const char* ptr = buf + sizeof(PROTOCOL);
 	int strsize = 0;
 	memcpy(&strsize, ptr, sizeof(int));
 	ptr += sizeof(int);
-	memcpy(nickname, ptr, strsize);
+	memcpy(msg, ptr, strsize);
+}
+void GetProtocol(const char* buf, PROTOCOL* protocol)
+{
+	const char* ptr = buf;
+	memcpy(protocol, ptr, sizeof(PROTOCOL));
 }
 int main(int argv, char* argc[])
 {
@@ -137,7 +182,7 @@ int main(int argv, char* argc[])
 	SOCKADDR_IN serveraddr;
 	ZeroMemory(&serveraddr, sizeof(serveraddr));
 	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_addr.s_addr = inet_addr(SERVERIP);
 	serveraddr.sin_port = htons(SERVERPORT);
 	retval = bind(listensock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
 	if (retval == SOCKET_ERROR)
@@ -155,14 +200,14 @@ int main(int argv, char* argc[])
 		if (sock == INVALID_SOCKET)
 			err_quit("accept");
 
-		printf("[TCP/IP 접속] IP: %s PORT: %d", inet_ntoa(serveraddr.sin_addr), ntohs(serveraddr.sin_port));
-		UserInfo user;
-		ZeroMemory(&user, sizeof(user));
-		user.sock = sock;
-		memcpy(&user.addr, &serveraddr, sizeof(serveraddr));
+		printf("[TCP/IP 접속] IP: %s PORT: %d\n", inet_ntoa(serveraddr.sin_addr), ntohs(serveraddr.sin_port));
+		UserInfo* user = new UserInfo();
+		ZeroMemory(user, sizeof(UserInfo));
+		user->sock = sock;
+		memcpy(&user->addr, &serveraddr, sizeof(serveraddr));
 
 		//스레드 생성
-		HANDLE hThread = CreateThread(0, NULL, ClientThread, (LPVOID)&user, NULL, 0);
+		HANDLE hThread = CreateThread(0, NULL, ClientThread, (LPVOID)user, NULL, 0);
 		if (hThread != NULL)
 			CloseHandle(hThread);
 
@@ -173,45 +218,202 @@ int main(int argv, char* argc[])
 }
 DWORD WINAPI ClientThread(LPVOID arg)
 {
-	UserInfo user = *(UserInfo*)arg;
+	UserInfo* user = (UserInfo*)arg;
+	UserInfo udp;
 	NetworkPacket packet;
 	ZeroMemory(&packet, sizeof(packet));
 	STATE state = STATE::START;
 	int size = 0;
 	int retval = 0;
-	switch (state)
-	{
-	case STATE::START:
-		// 닉네임 입력하시오 전송.
-		size = PackPacking(packet.Data, PROTOCOL::INPUTNICKNAME, NICKNAMEMSG);
-		retval = send(user.sock, packet.Data, size, 0);
-		if (retval == SOCKET_ERROR)
-			err_display("send()");
-		state = STATE::MENU;
-		break;
-	case STATE::MENU:
-		//닉네임 받아오고 메뉴 전송.
-		ZeroMemory(packet.Data, BUFSIZE);
-		if (!RecvPacket(user.sock, packet.Data))
-		{
-			printf("데이터 받기 실패");
-			return 0;
-		}
-		printf("[%s]님 접속");
-		ZeroMemory(packet.Data, BUFSIZE);
-		size = PackPacking(packet.Data, PROTOCOL::MENU, MENUSTRING);
-		retval = send(user.sock, packet.Data, size, 0);
-		if (retval == SOCKET_ERROR)
-			err_display("send()");
-		state=STATE::ADDR;
-		break;
-	case STATE::ADDR:
+	char buf[BUFSIZE];
+	int menunumber = 0;
 
-		break;
-
-	}
-
+	char IpArray[3][BUFSIZE] = { "224.1.1.1","224.1.1.2","224.1.1.3" };
+	int PortArray[3] = { 9001,9002,9003 };
 	
+	SOCKADDR_IN peeraddr;
+	ZeroMemory(&peeraddr, sizeof(peeraddr));
+	int peerlen = 0;
+	UdpPacket udppacket;
+	BOOL bEnable = TRUE;
+	ZeroMemory(&udppacket, sizeof(udppacket));
+	bool flag = false;
+	PROTOCOL protocol;
+	while (1)
+	{
+		if (flag&&udppacket.protocol!=PROTOCOL::MENU)
+			break;
+		switch (state)
+		{
+		case STATE::START:
+			// 닉네임 입력하시오 전송.
+			size = PackPacking(packet.Data, PROTOCOL::INPUTNICKNAME, NICKNAMEMSG);
+			retval = send(user->sock, packet.Data, size, 0);
+			if (retval == SOCKET_ERROR)
+				err_display("send()");
+			state = STATE::MENU;
+			break;
+		case STATE::MENU:
+			//닉네임 받아오고 메뉴 전송.
+			ZeroMemory(packet.Data, BUFSIZE);
+			if (udppacket.protocol != PROTOCOL::MENU && udppacket.protocol!=PROTOCOL::END)
+			{
+				if (!RecvPacket(user->sock, packet.Data))
+				{
+					printf("데이터 받기 실패  state=menu\n");
+					flag = true;
+					break;
+				}
+				GetProtocol(packet.Data, &protocol);
+				if (protocol == PROTOCOL::END)
+				{
+					state = STATE::END;
+					break;
+				}
+				ZeroMemory(buf, BUFSIZE);
+				UnPackPacket(packet.Data, buf);
+				printf("[%s]님 접속\n", buf);
+				ZeroMemory(packet.Data, BUFSIZE);
+			}
+			else if (udppacket.protocol == PROTOCOL::END)
+			{
+				state = STATE::END;
+				break;
+			}
+			//메뉴 전송
+			size = PackPacking(packet.Data, PROTOCOL::MENU, MENUSTRING);
+			retval = send(user->sock, packet.Data, size, 0);
+			if (retval == SOCKET_ERROR)
+				err_display("send()");
+			state = STATE::ADDR;
+			break;
+		case STATE::ADDR:
+		{	
+		ZeroMemory(packet.Data, BUFSIZE);
+		if (!RecvPacket(user->sock, packet.Data))
+		{
+			printf("데이터 받기 실패 state=addr\n");
+			flag = true;
+			break;
+		}
+		GetProtocol(packet.Data, &protocol);
+		if (protocol == PROTOCOL::END)
+		{
+			state = STATE::END;
+			break;
+		}
+		ZeroMemory(buf, BUFSIZE);
+		//메뉴 선택값에 따른 방 ip와 port 전송
+		UnPackPacket(packet.Data, buf);
+		menunumber = atoi(buf);
+		ZeroMemory(packet.Data, BUFSIZE);
+		switch (menunumber)
+		{
+		case 1:
+			size = PackPacking(packet.Data, PROTOCOL::ADDR, IpArray[0], &PortArray[0]);
+			retval = send(user->sock, packet.Data, size, 0);
+			if (retval == SOCKET_ERROR)
+				err_display("send()");
+			break;
+		case 2:
+			size = PackPacking(packet.Data, PROTOCOL::ADDR, IpArray[1], &PortArray[1]);
+			retval = send(user->sock, packet.Data, size, 0);
+			if (retval == SOCKET_ERROR)
+				err_display("send()");
+			break;
+		case 3:
+			size = PackPacking(packet.Data, PROTOCOL::ADDR, IpArray[2], &PortArray[2]);
+			retval = send(user->sock, packet.Data, size, 0);
+			if (retval == SOCKET_ERROR)
+				err_display("send()");
+			break;
+		default:
+			printf("잘못된 입력값");
+			//원래는 종료처리하지말고 에러메세지 클라에 전달. 밑의 return 0 은 임시코드
+			return 0;
+			break;
+		}
+		state = STATE::CHATTING;
+		break;
+		}
+		case STATE::CHATTING:
+			//udp 통신 소켓 생성
+			udp.sock = socket(AF_INET, SOCK_DGRAM, 0);
+			if (udp.sock == INVALID_SOCKET)
+				err_display("socket()");
+			udp.addr.sin_family = AF_INET;
+			udp.addr.sin_addr.s_addr = inet_addr(SERVERIP);
+			udp.addr.sin_port = htons(9005);
 
+			//해줘야 같은 주소로 다른 클라가 접근시 또 바인딩 할 수 있음.
+			retval = setsockopt(udp.sock, SOL_SOCKET, SO_REUSEADDR, (char*)&bEnable, sizeof(bEnable));
+			if (retval == SOCKET_ERROR)
+				err_quit("setsockopt()");
 
+			retval = bind(udp.sock, (SOCKADDR*)&udp.addr, sizeof(udp.addr));
+			if (retval == SOCKET_ERROR)
+				err_display("bind()");
+
+			//채팅시작
+			//while (1)
+			//{
+				ZeroMemory(&udppacket, sizeof(udppacket));
+				udppacket.protocol = PROTOCOL::NONE;
+				peerlen = sizeof(peeraddr);
+				//채팅종료 신호를 받아옵니다.
+				retval = recvfrom(udp.sock, (char*)&udppacket, sizeof(udppacket), 0, (SOCKADDR*)&peeraddr, &peerlen);
+				if (retval == SOCKET_ERROR)
+				{
+					err_display("recvfrom()");
+					state = STATE::MENU;
+					break;
+				}
+				//채팅종료 신호를 보낸 클라에 재 전송합니다.
+				retval = sendto(udp.sock, (char*)&udppacket, sizeof(udppacket), 0, (SOCKADDR*)&peeraddr, peerlen);
+				if (retval == SOCKET_ERROR)
+				{
+					err_display("sendto()");
+					state = STATE::MENU;
+					break;
+				}
+				//패킷에 전달된 프로토콜이 menu or end
+				if (udppacket.protocol == PROTOCOL::MENU || udppacket.protocol == PROTOCOL::END)
+				{
+					flag = true;
+				}
+			//}
+			//udp socket 반납
+			closesocket(udp.sock);
+			if (state == STATE::MENU)
+				continue;
+			//state를 메뉴로 변경
+			if (flag && udppacket.protocol == PROTOCOL::MENU)
+			{
+				state = STATE::MENU;
+				continue;
+			}
+			else if (flag)
+			{
+				state = STATE::END;
+			}
+			break;
+		case STATE::END:
+			flag = true;
+			break;
+		}
+	}
+	printf("[TCP/IP종료] ip:%s port:%d\n", inet_ntoa(user->addr.sin_addr), ntohs(user->addr.sin_port));
+	closesocket(user->sock);
+	return 0;
 }
+
+/* 문제점 한번 방에 들어갔다가 나왔을때 메뉴 출력까지는 잘 되는데 아직 클라에서 입력을 하지도 않았는데 state Add에서의 Recv()에서
+   recv가 진행된다. 왜일까.일단 제출하고 다시 생각해보기.
+   해야할것 방나가기,종료조건 좀더 좋게하기.*/
+
+/* 교수님 그냥 머리가 안돌아가구 잘 못하네요 죄송해요 ㅠㅠ 계속 tcp랑 udp할 헷갈려서 
+   채팅하다가 종료조건이 걸렸을때 채팅에서 서버udp주소로 send해서 protocol받고 받은 protocol을 다시 클라에 전송하는식으로 종료조건 주려고 했는데요
+   udp서버와 멀티캐스트 주소에 대한 개념이 이해가 제대로 되지 않았어서 한참 삽질하다가 서버의 udp 주소로 보내고 싶으면 서버 udp socket에 대한
+   ip,포트정보의 주소구조체에다가 보내야 한다는걸 깨달았네요.
+   이 외에도 지금 꼬여서 종료조건도 완벽하지 않고 방나가기 후 재 입장에 대한 구현이 덜 되었는데 다 해서 다시 올리겠습니다.
+   2022-03-31*/
